a33d4d7e60e77c4be9e8c173412eb2d40967d9b2
samarasena-iach <Samarasena.iach@gmail.com>
2025-04-23 17:12:14 +0530

    LeetCode #1 - Two Sum

---
+++ src/LeetCode_1_TwoSum.java
@@ -0,0 +1,48 @@
+import java.util.Arrays;
+
+/**
+ * LEETCODE - 1 - Two Sum
+ * <p>
+ * Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+ * You may assume that each input would have exactly one solution, and you may not use the same element twice.
+ * You can return the answer in any order.
+ * <p>
+ * Example 01
+ * Input: nums = [2,7,11,15], target = 9
+ * Output: [0,1]
+ * Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+ * <p>
+ * Example 02
+ * Input: nums = [3,2,4], target = 6
+ * Output: [1,2]
+ * <p>
+ * Example 03
+ * Input: nums = [3,3], target = 6
+ * Output: [0,1]
+ * Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.
+ **/
+
+public class LeetCode_1_TwoSum {
+    public static int[] twoSum(int[] nums, int target) {
+        int n = nums.length;
+        int[] pair = new int[2];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (nums[i] + nums[j] == target) {
+                    pair[0] = i;
+                    pair[1] = j;
+                }
+            }
+        }
+
+        return pair;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{3, 3};
+        int target = 6;
+
+        System.out.println(Arrays.toString(twoSum(nums, target)));
+    }
+}
